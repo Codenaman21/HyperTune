cdcd8ddf19efdcc05c982a211944c6e2a468d8f0
Naman Sachdeva <nsachdeva300@gmail.com>
2025-03-31 05:18:36 +0530

    Delete services directory

--- services/evaluate.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def evaluate_model(model_path):
-    # Dummy evaluation metrics
-    return {
-        "accuracy": np.random.uniform(80, 95),
-        "inference_time": np.random.uniform(0.1, 1.0)
-    }
